5e515bfb8552da551ea572d2dd9731397c5a7021
tgic <farmer1992@gmail.com>
2011-09-04 18:15:26 +0800

    chmod -x

--- dist.sh
+++ dist.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 git archive --prefix=MySlot/ --format=tar master | bzip2 > myslot.tar.bz2
+git archive --prefix=MySlot/ --format=zip master > myslot.zip
+
+md5sum myslot.tar.bz2 myslot.zip
